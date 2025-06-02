40ab42850cd7c1797bbcdf2c18b1ab73fe9efddc
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-06-02 00:31:46 -0300

    chore: Adiciona arquivos de configuração e auxiliares do projeto
    Inclui arquivos essenciais de configuração do Maven (.mvn/, pom.xml, mvnw, mvnw.cmd), .gitattributes, HELP.md e o diretório de testes.

---
+++ src/test/java/com/example/estoque_vendas/EstoqueVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estoque_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
