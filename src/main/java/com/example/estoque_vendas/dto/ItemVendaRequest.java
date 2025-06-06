eb3922895aeb5e0a70b0b02fe4cae2a7481779d3
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-06-06 02:07:04 -0300

    fix: Tratamento de Exceções

--- src/main/java/com/example/estoque_vendas/dto/ItemVendaRequest.java
+++ src/main/java/com/example/estoque_vendas/dto/ItemVendaRequest.java
@@ -3,12 +3,16 @@ package com.example.estoque_vendas.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ItemVendaRequest {
+    @NotNull(message = "O ID do produto não pode ser nulo.")
     private Long produtoId;
+    @NotNull(message = "A quantidade não pode ser nula.")
+    @Min(value = 1, message = "A quantidade deve ser maior que zero.")
     private Integer quantidade;
-    // O preço unitário será pego do Produto no Service
 }
