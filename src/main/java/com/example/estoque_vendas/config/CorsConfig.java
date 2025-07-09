966867a5821a6df86794a3730d4360f65157e2b2
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-07-09 00:45:19 -0300

    corrigindo a importação do cors

--- src/main/java/com/example/estoque_vendas/config/CorsConfig.java
+++ src/main/java/com/example/estoque_vendas/config/CorsConfig.java
@@ -3,16 +3,18 @@ package com.example.estoque_vendas.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
 
-@Configuration 
+
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Aplica as configurações CORS a todos os endpoints (**)
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:5500") // Substitua pela URL/porta do seu front-end
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Permite todos os métodos HTTP
-                .allowedHeaders("*") // Permite todos os cabeçalhos
-                .allowCredentials(true); // Permite o envio de credenciais (cookies, headers de autorização)
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:5500") // Altere conforme necessário
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
